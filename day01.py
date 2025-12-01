ce3b56ef489fa20ca9fb740a34dd9cec31a8e30f
karelians <45606302+karelinas@users.noreply.github.com>
2025-12-02 00:46:23 +0900

    Day 01 both parts in one file

--- day01.py
+++ day01.py
@@ -1,16 +1,13 @@
 from sys import stdin
 from typing import Callable
 
+INITIAL_DIAL: int = 50
+
 
 def main():
-    dial: int = 50
     instructions = parse_instruction(stdin.read())
-    zeros: int = 0
-    for inst in instructions:
-        zeros += count_zeros(dial, dial + inst)
-        dial = (dial + inst) % 100
-
-    print(zeros)
+    print("Part 1", count_zeros(instructions, count_if_stops_on_zero))
+    print("Part 2", count_zeros(instructions, count_all_zero_clicks))
 
 
 def parse_instruction(s: str) -> list[int]:
@@ -19,11 +16,22 @@ def parse_instruction(s: str) -> list[int]:
     return [get_sign(line) * get_int(line) for line in s.split("\n") if line]
 
 
-def count_zeros(start: int, end: int) -> int:
-    zeros = abs(end) // 100
-    if end < 0 and start != 0:
-        zeros += 1
-    if end == 0:
+def count_zeros(instructions: list[int], count_fn: Callable[[int, int], int]) -> int:
+    dial: int = INITIAL_DIAL
+    zeros: int = 0
+    for inst in instructions:
+        zeros += count_fn(dial, dial + inst)
+        dial = (dial + inst) % 100
+    return zeros
+
+
+def count_if_stops_on_zero(_: int, end: int) -> int:
+    return 1 if (end % 100) == 0 else 0
+
+
+def count_all_zero_clicks(start: int, end: int) -> int:
+    zeros: int = abs(end) // 100
+    if end == 0 or (end < 0 and start != 0):
         zeros += 1
     return zeros
 
