35d7f051e0e96e48824afb3f8f7e7fdc1c2287e4
Risto Karjalainen <karelianus@gmail.com>
2025-12-02 00:35:03 +0900

    Day 01 part 2

--- day01.py
+++ day01.py
@@ -7,10 +7,8 @@ def main():
     instructions = parse_instruction(stdin.read())
     zeros: int = 0
     for inst in instructions:
-        dial += inst
-        dial %= 100
-        if dial == 0:
-            zeros += 1
+        zeros += count_zeros(dial, dial + inst)
+        dial = (dial + inst) % 100
 
     print(zeros)
 
@@ -19,6 +17,15 @@ def parse_instruction(s: str) -> list[int]:
     get_sign: Callable[[str], int] = lambda x: -1 if x[0] == "L" else 1
     get_int: Callable[[str], int] = lambda x: int(x[1:])
     return [get_sign(line) * get_int(line) for line in s.split("\n") if line]
+
+
+def count_zeros(start: int, end: int) -> int:
+    zeros = abs(end) // 100
+    if end < 0 and start != 0:
+        zeros += 1
+    if end == 0:
+        zeros += 1
+    return zeros
 
 
 if __name__ == "__main__":
